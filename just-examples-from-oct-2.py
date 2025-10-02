5365b118efe0ce850be8054e884b7089d3713752
Joseph <josephnh@g.skku.edu>
2025-10-02 16:25:34 +0900

    Add just-examples-from-oct-2.py with various print statements and control structures

---
+++ just-examples-from-oct-2.py
@@ -0,0 +1,30 @@
+mylista = [1, 2, 3]
+print(mylista * 3)
+# Outputs: [1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+temperature = 20
+city = "Seoul"
+sunpower = 2000
+spffactor = 50
+message = f"You will be hit with sunpower {sunpower/spffactor}"
+message_2 = f"{city} is {temperature} degrees."
+
+print(temperature)
+print(message)
+print(message_2)
+
+listofspf = [25, 35, 50, 60]
+
+print(listofspf)
+
+for i in listofspf:
+    print("String")
+
+for i in listofspf:
+    print(i)
+
+if temperature < 35:
+    print("I am cool.")
+
+while temperature < 35:
+    print("I am cool.")
